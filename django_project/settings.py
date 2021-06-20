257eaf047aab0d058b528c5108b3257c2ebf0b1f
Kaustav Ray <kaustavray341@gmail.com>
2021-06-21 00:06:34 +0530

    Second message

--- django_project/settings.py
+++ django_project/settings.py
@@ -137,5 +137,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'kaustavray341@gmail.com'
-EMAIL_HOST_PASSWORD = 'utsav341'
+EMAIL_HOST_USER = os.environ.get('EMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('PASSWORD')
